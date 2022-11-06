98e60b4e5704d205b21b743f7b120d1d79b70934
RealOtakuBoi <sohamseth2005@gmail.com>
2022-11-06 18:44:24 +0530

    small change

--- pages/Components/Cart.jsx
+++ pages/Components/Cart.jsx
@@ -48,7 +48,7 @@ const Cart = () => {
               <div className="item-desc">
                 <div className="flex top">
                   <h5>{item.name}</h5>
-                  <h4>${item.price}</h4>
+                  <h4>₹{item.price}</h4>
                 </div>
                 <div className="flex bottom">
                   <div>
