ba8c7426e3fd91e852101e3b40ba9f1d2fbe144b
RealOtakuBoi <sohamseth2005@gmail.com>
2022-11-08 20:32:08 +0530

    working on payment

--- pages/api/Stripe.js
+++ pages/api/Stripe.js
@@ -12,7 +12,7 @@ export default async function handler(req , res){
             payment_method_types: ['card'],
             billing_address_collection: 'auto',
             shipping_options: [
-              { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+              { shipping_rate: '' },
             ],
             line_items: req.body.map((item) => {
               const img = item.image[0].asset._ref;
