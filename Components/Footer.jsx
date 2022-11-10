04b2d1a928c66355b1a7a10e91cac4c881eb81d7
RealOtakuBoi <sohamseth2005@gmail.com>
2022-11-10 23:05:28 +0530

    insta added

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { AiFillInstagram, AiOutlineTwitter, AiOutlineWhatsApp } from 'react-icons/ai'
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <div className='footer-container'>
       <p>2022 Saayo All Rights Reserved</p>
       <p className='icons'>
-        <AiFillInstagram />
+        <Link>
+        <AiFillInstagram href='https://www.instagram.com/saayo_store/?hl=en' />
+        </Link>
         <AiOutlineTwitter />
       </p>
       <p><AiOutlineWhatsApp/>+919870405708</p>
