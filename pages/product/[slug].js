bf569196870007b4eee55e781b96e848da38cb22
RealOtakuBoi <sohamseth2005@gmail.com>
2022-11-11 21:05:41 +0530

    sms substituted to whatsapp

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -60,7 +60,7 @@ const ProductDeatils = ({ product , products}) => {
         </div>
         <div className="buttons">
           <button type="button" className="add-to-cart" onClick={() => onAdd(product , qty)} >Add to Cart</button>
-          <a href={`sms://+919870405708;?&body=I%20Want%20Buy%20${name}%20To%20Priced%20at%20${price}`}>
+          <a href={`https://wa.me/+919870405708?text=Hello%20I%20want%20to%20Purchase%20${name}%20priced%20at%20${price}`}>
           <button type="button" className="buy-now">Buy Now</button>
           </a>
         </div>
