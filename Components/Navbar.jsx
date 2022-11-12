cc3e13093b2cb193be6e0ca971392e0ddf5556dc
RealOtakuBoi <sohamseth2005@gmail.com>
2022-11-12 15:45:52 +0530

    bottom navbar added

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { AiOutlineShopping, AiOutlineWhatsApp } from 'react-icons/ai'
+import { AiOutlineHome, AiOutlinePhone, AiOutlineShop, AiOutlineShopping, AiOutlineWhatsApp } from 'react-icons/ai'
 import  Cart  from './Cart';
 import { useStateContext } from '../Context/StateContext';
 
@@ -8,6 +8,7 @@ const Navbar = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return (
+    <>
     <div className="navbar-container">
       <p className="logo">
         <Link href="/">Saayo</Link>
@@ -30,6 +31,20 @@ const Navbar = () => {
 
       {showCart && <Cart />}
     </div>
+
+    <div className='bottom-navbar'>
+    <a><Link href="/"><AiOutlineHome/></Link></a>
+    <a><Link href={`/sell/sellForm`}><AiOutlineShop/></Link></a>
+    <a><Link href={`https://wa.me/+919870405708?text=Hello%20I%20want%20to%20Contact%20you%20About%20some%20queries`}><AiOutlinePhone/></Link></a>
+    <a><button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
+
+      {showCart && <Cart />}</a>
+    </div>
+    </>
+
   )
 }
 
