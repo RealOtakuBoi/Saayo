ac84224f86e24126a6afafd0ce14e082e0ad6d23
RealOtakuBoi <sohamseth2005@gmail.com>
2022-11-13 21:34:38 +0530

    sell form works

--- pages/sell/sellForm.js
+++ pages/sell/sellForm.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Script from 'next/script'
+import emailjs from '@emailjs/browser';
 
 const sellForm = () => {
 
@@ -18,42 +19,63 @@ const sellForm = () => {
 // }
 
 
+const form = useRef();
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs.sendForm('service_6gl4a4j', 'template_0g21d3c', form.current, 'VAKUikMKqrmcm4Nrw')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+};
+
+
   return (
 
     <>
     <div className='pt1'>
         <div className='sell-container'>
         <div className='sell-title'>Connect with Us</div>
-        <form action="#" className='sell-content'>
+        <form ref={form} onSubmit={sendEmail} className='sell-content'>
             <div className='sell-user-details'>
                 <div className='sell-input-box'>
                     <span className='sell-details'>Full Name</span>
-                    <input type="text" name="name" placeholder='Enter Your Full Name' id="name" required/>
+                    <input type="text" name="user_name" placeholder='Enter Your Full Name' id="name" required/>
                 </div>
             </div>
 
             <div className='sell-user-details'>
                 <div className='sell-input-box'>
                     <span className='sell-details'>Phone Number</span>
-                    <input type="tel" name="phone" id="num" placeholder='enter your phone number' required  />
+                    <input type="tel" name="number" id="num" placeholder='enter your phone number' required  />
                 </div>
             </div>
 
             <div className='sell-user-details'>
                 <div className='sell-input-box'>
                     <span className='sell-details'>Adress</span>
-                    <input type="text" name="Address" id="adress" required  />
+                    <input type="text" name="address" id="adress" required  />
                 </div>
             </div>
 
             <div className='sell-user-details'>
+                <div className='sell-input-box'>
+                    <span className='sell-details'>Book Name</span>
+                    <input type="text" name="book" id="bookname" required  />
+                </div>
+            </div>
+
+            {/* <div className='sell-user-details'>
                 <div className='sell-input-box'>
                 <select name="seller" id="seller" class="sell-box">
                     <option value="individual">Individual</option>
                     <option value="store">Store</option>
                 </select>
                 </div>
-            </div>
+            </div> */}
 
             <button className='sell-btn'>
                 Submit
